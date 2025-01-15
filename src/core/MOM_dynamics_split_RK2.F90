50d5cf0f371c819eacabbb62109c821827277f04
Edward D. Zaron <edward.d.zaron@oregonstate.edu>
2025-01-15 16:55:55 -0500

    MOM_dynamics_split_RK2.F90

--- src/core/MOM_dynamics_split_RK2.F90
+++ src/core/MOM_dynamics_split_RK2.F90
@@ -47,7 +47,7 @@ use MOM_CoriolisAdv,           only : CorAdCalc, CoriolisAdv_CS
 use MOM_CoriolisAdv,           only : CoriolisAdv_init, CoriolisAdv_end
 use MOM_debugging,             only : check_redundant
 use MOM_grid,                  only : ocean_grid_type
-use MOM_harmonic_analysis,     only : harmonic_analysis_CS
+use MOM_harmonic_analysis,     only : harmonic_analysis_CS, harmonic_analysis_CS_edz
 use MOM_hor_index,             only : hor_index_type
 use MOM_hor_visc,              only : horizontal_viscosity, hor_visc_CS
 use MOM_hor_visc,              only : hor_visc_init, hor_visc_end
@@ -246,6 +246,7 @@ type, public :: MOM_dyn_split_RK2_CS ; private
   type(tidal_forcing_CS) :: tides_CSp
   !> A pointer to the harmonic analysis control structure
   type(harmonic_analysis_CS) :: HA_CSp
+  type(harmonic_analysis_CS_edz) :: HA_CSp_edz
   !> A pointer to the ALE control structure.
   type(ALE_CS), pointer :: ALE_CSp => NULL()
 
@@ -1311,7 +1312,7 @@ end subroutine remap_dyn_split_RK2_aux_vars
 !> This subroutine initializes all of the variables that are used by this
 !! dynamic core, including diagnostics and the cpu clocks.
 subroutine initialize_dyn_split_RK2(u, v, h, tv, uh, vh, eta, Time, G, GV, US, param_file, &
-                      diag, CS, HA_CSp, restart_CS, dt, Accel_diag, Cont_diag, MIS, &
+                      diag, CS, HA_CSp, HA_CSp_edz, restart_CS, dt, Accel_diag, Cont_diag, MIS, &
                       VarMix, MEKE, thickness_diffuse_CSp,                  &
                       OBC, update_OBC_CSp, ALE_CSp, set_visc, &
                       visc, dirs, ntrunc, pbv, calc_dtbt, cont_stencil)
@@ -1335,7 +1336,8 @@ subroutine initialize_dyn_split_RK2(u, v, h, tv, uh, vh, eta, Time, G, GV, US, p
   type(diag_ctrl),          target, intent(inout) :: diag       !< to control diagnostics
   type(MOM_dyn_split_RK2_CS),       pointer       :: CS         !< module control structure
   type(harmonic_analysis_CS),       pointer       :: HA_CSp     !< A pointer to the control structure of the
-                                                                !! harmonic analysis module
+  type(harmonic_analysis_CS_edz),       pointer       :: HA_CSp_edz     !< A pointer to the control structure of the
+  !! harmonic analysis module
   type(MOM_restart_CS),             intent(inout) :: restart_CS !< MOM restart control structure
   real,                             intent(in)    :: dt         !< time step [T ~> s]
   type(accel_diag_ptrs),    target, intent(inout) :: Accel_diag !< points to momentum equation terms for
@@ -1508,8 +1510,9 @@ subroutine initialize_dyn_split_RK2(u, v, h, tv, uh, vh, eta, Time, G, GV, US, p
   call CoriolisAdv_init(Time, G, GV, US, param_file, diag, CS%ADp, CS%CoriolisAdv)
   if (CS%calculate_SAL) call SAL_init(G, US, param_file, CS%SAL_CSp)
   if (CS%use_tides) then
-    call tidal_forcing_init(Time, G, US, param_file, CS%tides_CSp, CS%HA_CSp)
+    call tidal_forcing_init(Time, G, US, param_file, CS%tides_CSp, CS%HA_CSp, CS%HA_CSp_edz)
     HA_CSp => CS%HA_CSp
+    HA_CSp_edz => CS%HA_CSp_edz
   else
     HA_CSp => NULL()
   endif
